602ad9311921feffc3c81c8a70d41c29acbe3450
张清阳 <18737210295@163.com>
2020-11-17 22:05:09 +0800

    update

--- src/config/axios.js
+++ src/config/axios.js
@@ -6,7 +6,7 @@ export const getRequest = (url, params) => {
         url: `${url}`,
         params: params,
         headers: {
-            'Authorization' : 'eyJ0eXAiOiJKV1QiLCJhbiOiJIUzI1NiJ9.eyJzdWIiOiIxMjIwMTg2MTAwQHFxLmNvbSIsImV4cCI6MTYwNTYwMTAwNywiaWF0IjoxNjA1NTk3NDA3fQ.FbvAxahpQFE_4Pospg8P9M7R5ozG9bbcLk6RAETutgA'
+            'Authorization' : '\teyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjIwMTg2MTAwQHFxLmNvbSIsImV4cCI6MTYwNTYyNTQ4NiwiaWF0IjoxNjA1NjIxODg2fQ.ZzUHAdgMJGipQTX9_IsW60pE5OLHkbfgQnK-kcJcaPM'
         }
     });
 };
