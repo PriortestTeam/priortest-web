27567247bb7bc94694f271f6a332c6c748de8a03
张清阳 <18737210295@163.com>
2020-11-17 22:27:56 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,12 @@
 module.exports = {
 
     devServer: {
-        proxy: {
-            '/': {
-                target: 'http://106.13.39.32:8081/api',
-                changeOrigin: true
-            }
-        }
+        // proxy: {
+        //     '/': {
+        //         target: 'http://106.13.39.32:8081/api',
+        //         changeOrigin: true
+        //     }
+        // }
     }
 
 };
