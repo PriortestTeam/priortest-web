7427727b903f9ff873d96e98d795ef6ca1baca60
单波波 <s302702977@163.com>
2021-02-05 09:48:44 +0800

    eslint

--- src/views/project/index.vue
+++ src/views/project/index.vue
@@ -99,121 +99,122 @@
           <div v-if="setTree.length === 0" class="nodata">暂无数据</div>
         </div>
       </el-col>
-      <el-col :span="19"
-        ><div class="project_table">
-          <div class="oprate_btn">
-            <el-button type="text" @click="projectRefresh">刷新</el-button>
-            <el-button type="text" :disabled="single" @click="projectChange"
-              >切换项目</el-button
-            >
-            <el-button type="text" :disabled="single" @click="projectClone"
-              >克隆</el-button
-            >
-            <el-button
-              type="text"
-              :disabled="multiple"
-              @click="delproject('all')"
-              >批量删除</el-button
-            >
-            <!-- <el-button type="text" :disabled="multiple">批量编辑</el-button> -->
-          </div>
-          <div class="protable table">
-            <el-table
-              ref="projecttableData"
-              :data="projecttableData"
-              :header-cell-style="tableHeader"
-              stripe
-              style="width: 100%"
-              @row-click="switcproject"
-              @selection-change="handleSelectionChange"
-            >
-              <el-table-column type="selection" width="55" />
-              <el-table-column type="index" align="center" label="序号">
-                <template slot-scope="scope">
-                  {{ scope.$index + 1 }}
-                </template>
-              </el-table-column>
-              <el-table-column
-                prop="title"
-                :show-overflow-tooltip="true"
-                align="center"
-                label="标题"
-              />
-              <el-table-column
-                prop="reportToName"
-                align="center"
-                label="负责人"
-              />
-              <el-table-column prop="status" align="center" label="状态">
-                <template slot-scope="scope">
-                  <span>{{
-                    scope.row.status === 1
-                      ? "Progress"
-                      : scope.row.status === 2
+      <el-col
+        :span="19"
+      ><div class="project_table">
+        <div class="oprate_btn">
+          <el-button type="text" @click="projectRefresh">刷新</el-button>
+          <el-button
+            type="text"
+            :disabled="single"
+            @click="projectChange"
+          >切换项目</el-button>
+          <el-button
+            type="text"
+            :disabled="single"
+            @click="projectClone"
+          >克隆</el-button>
+          <el-button
+            type="text"
+            :disabled="multiple"
+            @click="delproject('all')"
+          >批量删除</el-button>
+          <!-- <el-button type="text" :disabled="multiple">批量编辑</el-button> -->
+        </div>
+        <div class="protable table">
+          <el-table
+            ref="projecttableData"
+            :data="projecttableData"
+            :header-cell-style="tableHeader"
+            stripe
+            style="width: 100%"
+            @row-click="switcproject"
+            @selection-change="handleSelectionChange"
+          >
+            <el-table-column type="selection" width="55" />
+            <el-table-column type="index" align="center" label="序号">
+              <template slot-scope="scope">
+                {{ scope.$index + 1 }}
+              </template>
+            </el-table-column>
+            <el-table-column
+              prop="title"
+              :show-overflow-tooltip="true"
+              align="center"
+              label="标题"
+            />
+            <el-table-column
+              prop="reportToName"
+              align="center"
+              label="负责人"
+            />
+            <el-table-column prop="status" align="center" label="状态">
+              <template slot-scope="scope">
+                <span>{{
+                  scope.row.status === 1
+                    ? "Progress"
+                    : scope.row.status === 2
                       ? "Plan"
                       : "Closed"
-                  }}</span>
-                </template>
-              </el-table-column>
+                }}</span>
+              </template>
+            </el-table-column>
 
-              <el-table-column
-                prop="createTime"
-                align="center"
-                label="创建日期"
-                min-width="120"
-                :show-overflow-tooltip="true"
-              />
-              <el-table-column
-                prop="planReleaseDate"
-                align="center"
-                label="计划上线日期"
-                min-width="120"
-                :show-overflow-tooltip="true"
-              >
-                <template slot-scope="scope">
-                  <span>{{ scope.row.planReleaseDate || "待定" }}</span>
-                </template>
-              </el-table-column>
-              <el-table-column
-                prop="closeDate"
-                align="center"
-                label="关闭日期"
-                min-width="120"
-                :show-overflow-tooltip="true"
-              >
-                <template slot-scope="scope">
-                  <span>{{ scope.row.closeDate || "待定" }}</span>
-                </template>
-              </el-table-column>
-              <el-table-column label="操作" min-width="120" align="center">
-                <template slot-scope="scope">
-                  <!-- <el-button type="text" class="table-btn">克隆</el-button>
-                <span class="line">|</span> -->
-                  <el-button
-                    type="text"
-                    class="table-btn"
-                    @click.stop="openEdit(scope.row)"
-                    >编辑</el-button
-                  >
-                  <el-button
-                    type="text"
-                    class="table-btn"
-                    @click.stop="delproject(scope.row.id)"
-                    >删除</el-button
-                  >
-                </template>
-              </el-table-column>
-            </el-table>
-
-            <pagination
-              v-show="projectTotal > 0"
-              :total="projectTotal"
-              :page.sync="projectQuery.pageNum"
-              :limit.sync="projectQuery.pageSize"
-              @pagination="getqueryForProjects"
+            <el-table-column
+              prop="createTime"
+              align="center"
+              label="创建日期"
+              min-width="120"
+              :show-overflow-tooltip="true"
             />
-          </div></div
-      ></el-col>
+            <el-table-column
+              prop="planReleaseDate"
+              align="center"
+              label="计划上线日期"
+              min-width="120"
+              :show-overflow-tooltip="true"
+            >
+              <template slot-scope="scope">
+                <span>{{ scope.row.planReleaseDate || "待定" }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column
+              prop="closeDate"
+              align="center"
+              label="关闭日期"
+              min-width="120"
+              :show-overflow-tooltip="true"
+            >
+              <template slot-scope="scope">
+                <span>{{ scope.row.closeDate || "待定" }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="操作" min-width="120" align="center">
+              <template slot-scope="scope">
+                <!-- <el-button type="text" class="table-btn">克隆</el-button>
+                <span class="line">|</span> -->
+                <el-button
+                  type="text"
+                  class="table-btn"
+                  @click.stop="openEdit(scope.row)"
+                >编辑</el-button>
+                <el-button
+                  type="text"
+                  class="table-btn"
+                  @click.stop="delproject(scope.row.id)"
+                >删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+
+          <pagination
+            v-show="projectTotal > 0"
+            :total="projectTotal"
+            :page.sync="projectQuery.pageNum"
+            :limit.sync="projectQuery.pageSize"
+            @pagination="getqueryForProjects"
+          />
+        </div></div></el-col>
     </el-row>
   </div>
 </template>
@@ -409,12 +410,12 @@ export default {
     // 表格行点击去编辑
     openEdit(row) {
       const data = JSON.stringify(row)
-      this.$router.push({ name: 'Addproject', query: { info: data } })
+      this.$router.push({ name: 'Addproject', query: { info: data }})
     },
     // 新建项目
     newproject() {
       const data = JSON.stringify({ status: '3' })
-      this.$router.push({ name: 'Addproject', query: { info: data } })
+      this.$router.push({ name: 'Addproject', query: { info: data }})
     },
     // 项目切换
     switcproject(row) {
