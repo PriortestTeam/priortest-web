856532bd71784b1d509605a38743449332c2e72f
szz19960815 <1316225239@qq.com>
2022-01-18 17:12:14 +0800

    上传签名调整，限制大小，限制类型

--- src/components/Upload/UploadSigenatrue.vue
+++ src/components/Upload/UploadSigenatrue.vue
@@ -1,14 +1,17 @@
 <template>
   <el-upload
+    ref="upload"
     class="upload-demo"
+    accept=".jpg,.jpeg,.png"
     :action="url"
     :auto-upload="false"
     :headers="headers"
+    :show-file-list="false"
     :on-change="handleChange"
   >
     <el-button type="primary" size="small">上传签名</el-button>
     <div slot="tip" class="el-upload__tip">
-      一次只能上传一个文件
+      仅支持JPG和PNG格式，且不超过5MB，一次只能上传一个文件
     </div>
   </el-upload>
 </template>
@@ -16,29 +19,38 @@
 <script>
 import { uploadSignature } from '../../api/signoff.js'
 export default {
-  props:['signPaath'],
-  computed: {
-    headers() {
-      return {
-        Authorization: 'Bearer ' + localStorage.getItem('token')//本地获取token,添加到headers里面
-      }
+  props: ['signPaath'],
+  data () {
+    return {
+      url: process.env.VUE_APP_BASE_API + '/signOff/upload'
     }
   },
-  data() {
-    return {
-      url:process.env.VUE_APP_BASE_API+'/signOff/upload'
+  computed: {
+    headers () {
+      return {
+        Authorization: 'Bearer ' + localStorage.getItem('token')// 本地获取token,添加到headers里面
+      }
     }
   },
   methods: {
     handleChange(file, fileList) {
-      const formData = new FormData();
-      formData.append("file", file.raw);
-      uploadSignature(formData).then(res => {
-        this.$emit('getImgUrl', res);
-      })
-    },
+      console.log(file)
+      if (file.raw.type !== 'image/png' && file.raw.type !== 'image/jpg' || file.raw.type !== 'image/jpeg') {
+        this.$message.warning('仅支持JPG和PNG格式')
+        this.$refs.upload.clearFiles()
+      } else if (file.size > 1024 * 1024 * 5) {
+        this.$message.warning('上传文件大小不能超过5MB')
+        this.$refs.upload.clearFiles()
+      } else {
+        const formData = new FormData()
+        formData.append('file', file.raw)
+        uploadSignature(formData).then(res => {
+          this.$emit('getImgUrl', res)
+        })
+      }
+    }
   }
-};
+}
 </script>
 
 <style></style>
